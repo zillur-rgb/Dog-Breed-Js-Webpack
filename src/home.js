87de16c85fbec4946251e388b058a05ff8eece92
zillur-rgb <zillurdeu@gmail.com>
2022-03-14 16:31:14 +0100

    set footer elements

---
+++ src/home.js
@@ -0,0 +1,16 @@
+const home = (() => {
+  const content = document.createElement("div");
+  content.classList.add("title-container");
+
+  const titleWrapper = document.createElement("div");
+  titleWrapper.classList.add("title");
+
+  const title = document.createElement("h1");
+  const infoWrapper = document.createElement("div");
+  infoWrapper.classList.add("info-container");
+
+  const card = document.createElement("div");
+  card.classList.add("card radius");
+
+  return {};
+})();
--- src/panel.js
+++ src/panel.js
@@ -1,8 +1,13 @@
 const panels = (() => {
+  // header Variables
   let header = document.createElement("header");
   let nav = document.createElement("nav");
   let links = document.createElement("a");
   let body = document.querySelector("body");
+
+  // Footer variables
+  const footer = document.createElement("footer");
+  const github = document.createElement("div");
 
   const appendHeader = () => {
     header.classList.add("panel");
@@ -21,7 +26,11 @@ const panels = (() => {
   };
 
   const appendFooter = () => {
-    console.log("footer");
+    footer.classList.add("panel");
+    github.setAttribute("href", "https://github.com/zillur-rgb");
+    github.innerHTML = `<p>copyright 2022 MD Zillur Rahman</p>`;
+    body.append(footer);
+    document.querySelector("footer").append(github);
   };
 
   return {
